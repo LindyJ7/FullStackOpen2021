cf321473bea305f6c84d871c510531ae857bd27e
Lindy Janowiak <linderminder@hotmail.com>
2022-02-16 22:39:30 -0600

    Part1c state changes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <h1>{course.name}</h1>
-    <Header name={course.parts[0].name} exercises={course.parts[0].exercises} />
-    <Header name={course.parts[1].name} exercises={course.parts[1].exercises} />
-    <Header name={course.parts[2].name} exercises={course.parts[2].exercises} />
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
 
-const Header = (props) => {
-  console.log(props)
-  const name = props.name;
+const Display = ({counter}) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+
+const Button = ({onClick, text}) => {
   return (
     <div>
-      <p>
-      {name} has {props.exercises} exercises
-      </p>
+      <button onClick={onClick}>
+        {text}
+      </button>
     </div>
   )
 }

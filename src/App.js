ff1d655baa939d1afb5d575d04b70f437733f337
Lindy Janowiak <linderminder@hotmail.com>
2022-02-16 22:50:18 -0600

    Click tracking

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />           
+      {left}
+      <Button onClick={handleLeftClick} text="left"/>
+      <Button onClick={handleRightClick} text="right"/>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
-  )
-}
-
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
   )
 }
 
 const Button = ({onClick, text}) => {
   return (
     <div>
-      <button onClick={onClick}>
-        {text}
-      </button>
+      <button onClick={onClick}> {text} </button>
     </div>
   )
 }

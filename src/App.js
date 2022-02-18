67e1956c854ca821b2f9968b77a26678f18f1eb2
Lindy Janowiak <linderminder@hotmail.com>
2022-02-17 18:45:58 -0600

    Buttons + history components

--- src/App.js
+++ src/App.js
@@ -21,7 +21,22 @@ const App = () => {
       <Button onClick={handleLeftClick} text="left"/>
       <Button onClick={handleRightClick} text="right"/>
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+const History = ({allClicks}) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {allClicks.join(' ')}
     </div>
   )
 }
